ce74ca5f9dd8c693ec67f6e5b27ca5f4d8ec057f
utkirbek <utkirbek_qutfiddinov@mail.ru>
2023-07-12 07:50:41 +0500

    Somer users have been added with in MemoryAuthentication

--- src/main/java/uz/utkirbek/springbootcrudwithsecurity/config/SecurityConfig.java
+++ src/main/java/uz/utkirbek/springbootcrudwithsecurity/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package uz.utkirbek.springbootcrudwithsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Muallif: Utkirbek
@@ -23,5 +27,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("director").password(passwordEncoder().encode("director")).roles("DIRECTOR")
+                    .and()
+                    .withUser("manager").password(passwordEncoder().encode("manager")).roles("MANAGER")
+                    .and()
+                    .withUser("worker").password(passwordEncoder().encode("worker")).roles("WORKER");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
